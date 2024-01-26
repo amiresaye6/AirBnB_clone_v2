abac2f8583445fad9eda6ee0d03a8f612c9d2ce4
Amir Alsayed <amyralsyd367@gmail.com>
2024-01-26 15:04:01 +0200

    pycodestyle

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,13 +3,14 @@
 A script that starts a Flask web application:
 """
 from flask import Flask
-
 airpnp_app = Flask(__name__)
 
-@airpnp_app.route("/", strict_slashes=False)
 
+@airpnp_app.route("/", strict_slashes=False)
 def home():
-	return ("Hello HBNB!")
+    return ("Hello HBNB!")
+
 
 if __name__ == "__main__":
-	airpnp_app.run(debug=True, host="0.0.0.0",port=5000)
+
+    airpnp_app.run(debug=True, host="0.0.0.0", port=5000)
