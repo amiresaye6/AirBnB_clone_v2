3985347f4c5cb1dcfb33048230a337106053cadc
Amir Alsayed <amyralsyd367@gmail.com>
2024-01-28 22:14:15 +0200

    idk

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -3,7 +3,7 @@
 
 The application listens on 0.0.0.0, port 5000.
 Routes:
-    /states_list: HTML page with a list of all State objects in DBStorage.
+    /cities_by_states: HTML page with a list of all State objects in DBStorage.
 """
 from models import storage
 from flask import Flask, render_template
@@ -11,10 +11,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
-def states_list():
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
     states = storage.all("State")
-    return render_template("7-states_list.html", states=states)
+    return render_template("8-cities_by_states.html", states=states)
 
 
 @app.teardown_appcontext
